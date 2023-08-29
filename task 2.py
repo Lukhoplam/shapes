7b96379e5d5d4799cf433cc15b0113aa4ef51b4f
lukho <plamlukho1@gmail.com>
2023-08-29 22:35:00 +0200

    letter input removed

--- task 2.py
+++ task 2.py
@@ -1,22 +1,20 @@
-shape=input(str("choose shape number of the building you want \n 1.square\n2.rectangular\n3.round\n")).lower()
+shape =int(input("choose shape number of the building you want\n1.square\n2.rectangular\n3.round\n"))
 
 
 pi_=3.142
 
 print()
-if shape=="1" :
+if shape == "1" :
     length=(float(input(" what is the length of the building?")))
     print(length**2)
 
-elif shape=='2':
+elif shape == '2':
     width=(float(input("what is the width of the building ?")))
     length=(float(input(" what is the length of the building?")))
     print(length*width)
 
-else:
-    shape=='3'
+elif shape == '3':
     raduis=(float(input('what is the radius ?')))
-    
     print(pi_*(raduis**2))
     
     
